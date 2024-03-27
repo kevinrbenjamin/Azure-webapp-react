3f40c8593d9f13fb8356b66ffd7abe1dd9a52331
unknown <kevin.r.benjamin@gmail.com>
2024-03-26 20:43:22 -0400

    Update1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test for Azure Website
+          Test for Azure Website #2
         </p>
         <a
           className="App-link"
